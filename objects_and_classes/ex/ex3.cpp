b805be01e228f33a5535d34c07ab5576def454cf
Denis M <denis.m.031997@gmail.com>
2026-08-10 13:32:31 +0300

    objects_and_classes->added ex3;

---
+++ objects_and_classes/ex/ex3.cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <vector>
+
+using std::string;
+
+class Sale
+{
+private:
+    string city_name;
+    string product_type;
+    double price_per_unit;
+    int quantity;
+
+public:
+    // def c-tor
+    // Sale();
+    // c-tor
+    // params c-tor
+    Sale(const string &city_name, const string &product_type,
+         const double &price_per_unit, const int &quantity);
+    // copy-c-tor
+    Sale(const Sale &obj_data);
+    // assignment operator overload
+    Sale &operator=(const Sale &obj_data);
+
+    // getters and setters
+    const string &getterCityName() const;
+    const string &getterProductType() const;
+    const double &getterPricePerUnit() const;
+    const int &getterQuantity() const;
+
+    void setterProductQuantity(const int &value_variable);
+};
+
+Sale::Sale(const string &city_name, const string &product_type,
+           const double &price_per_unit, const int &quantity)
+{
+    this->city_name = city_name;
+    this->product_type = product_type;
+    this->price_per_unit = price_per_unit;
+    this->quantity = quantity;
+}
+
+Sale::Sale(const Sale &obj_data)
+{
+    this->city_name = obj_data.city_name;
+    this->product_type = obj_data.product_type;
+    this->price_per_unit = obj_data.price_per_unit;
+    this->quantity = obj_data.quantity;
+}
+
+Sale &Sale::operator=(const Sale &obj_data)
+{
+    if (this != &obj_data)
+    {
+        this->city_name = obj_data.city_name;
+        this->product_type = obj_data.product_type;
+        this->price_per_unit = obj_data.price_per_unit;
+        this->quantity = obj_data.quantity;
+    }
+
+    return *this;
+}
+
+const string &Sale::getterCityName() const
+{
+    return this->city_name;
+}
+
+const string &Sale::getterProductType() const
+{
+    return this->product_type;
+}
+
+const double &Sale::getterPricePerUnit() const
+{
+    return this->price_per_unit;
+}
+
+const int &Sale::getterQuantity() const
+{
+    return this->quantity;
+}
+
+void Sale::setterProductQuantity(const int &value_variable)
+{
+    this->quantity = value_variable;
+}
+
+class ProfitTracker
+{
+private:
+    std::map<string, std::vector<Sale>> inner_cont;
+
+    Sale *getExistingProduct(std::vector<Sale> &curr_product_cont, const Sale &obj_inst);
+
+public:
+    void insertSaleToProfitTracker(const Sale &obj_inst);
+    void printProfitData();
+};
+
+void ProfitTracker::insertSaleToProfitTracker(const Sale &obj_inst)
+{
+    std::map<string, std::vector<Sale>>::iterator iter_specific_obj =
+        this->inner_cont.find(obj_inst.getterCityName());
+
+    bool does_key_exist = iter_specific_obj != this->inner_cont.end();
+    if (!does_key_exist)
+    {
+        std::pair<string, std::vector<Sale>> curr_pair(obj_inst.getterCityName(), std::vector<Sale>{obj_inst});
+
+        std::pair<std::map<std::string, std::vector<Sale>>::iterator, bool> iter_kvp =
+            this->inner_cont.insert(curr_pair);
+    }
+    else
+    {
+        const string &kvp_key = iter_specific_obj->first;
+        std::vector<Sale> &kvp_value = iter_specific_obj->second;
+
+        Sale *ptr_existing_obj = this->getExistingProduct(kvp_value, obj_inst);
+        if (ptr_existing_obj != nullptr)
+        {
+            ptr_existing_obj->setterProductQuantity(ptr_existing_obj->getterQuantity() +
+                                                    obj_inst.getterQuantity());
+        }
+        else
+        {
+            kvp_value.push_back(obj_inst);
+        }
+    }
+}
+
+Sale *ProfitTracker::getExistingProduct(std::vector<Sale> &curr_product_cont,
+                                        const Sale &obj_inst)
+{
+    for (int idx = 0; idx < curr_product_cont.size(); idx++)
+    {
+        Sale &curr_obj = curr_product_cont[idx];
+        if (curr_obj.getterProductType() == obj_inst.getterProductType())
+        {
+            return &curr_obj;
+        }
+    }
+
+    /*
+    for (Sale curr_obj : curr_product_cont)
+    {
+        if (curr_obj.getterProductType() == obj_inst.getterProductType())
+        {
+            return &curr_obj;
+        }
+    }
+    */
+
+    return nullptr;
+}
+
+void ProfitTracker::printProfitData()
+{
+    for (const std::pair<string, std::vector<Sale>> &curr_kvp : this->inner_cont)
+    {
+        const string &kvp_key = curr_kvp.first;
+        const std::vector<Sale> &kvp_val = curr_kvp.second;
+
+        double district_total_profit = 0;
+        for (const Sale &curr_obj : kvp_val)
+        {
+            double curr_product_total = curr_obj.getterPricePerUnit() * curr_obj.getterQuantity();
+            district_total_profit += curr_product_total;
+        }
+
+        std::cout << kvp_key << " -> " << district_total_profit << std::endl;
+    }
+}
+
+int main()
+{
+    // ProfitTracker *ptr_profit_tracker = new ProfitTracker();
+
+    int num_lines;
+    std::cin >> num_lines;
+
+    for (int idx = 0; idx < num_lines; idx++)
+    {
+        std::cout << idx << std::endl;
+
+        string curr_data_line;
+        std::getline(std::cin, curr_data_line);
+        std::stringstream str_stream(curr_data_line);
+
+        string curr_city_name;
+        string curr_product_type;
+        double curr_price_per_unit;
+        int curr_quantity;
+
+        str_stream >> curr_city_name >> curr_product_type >> curr_price_per_unit >> curr_quantity;
+    }
+
+    return 0;
+}
